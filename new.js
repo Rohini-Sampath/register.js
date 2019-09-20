cdd50b3823642e807ca9133addba918e1b168d37
Rohini <rohinisampath2626@gmail.com>
2019-09-20 22:18:42 +0530

    initial commit of a new form

---
+++ new.js
@@ -0,0 +1,10 @@
+function check(form) {
+  var nameValue = document.getElementById("userid").value;
+    console.log(nameValue);
+    
+    // console.log(form.name.value);
+    console.log(form.num.value);
+    console.log(form.married.value);
+    console.log(form.male.value);
+    console.log(form.sub.value);
+}
